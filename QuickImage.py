47922807da539c88207ad9bfaa9e72a4ef6c2488
William Nascimento <william08.dcc@gmail.com>
2023-05-19 01:44:42 -0300

    first functional version

--- QuickImage.py
+++ QuickImage.py
@@ -1,5 +1,5 @@
 __author__ = 'William Nascimento'
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 __maintainer__ = "William Nascimento"
 __status__ = "Development"
 
@@ -7,6 +7,7 @@ __status__ = "Development"
 import cv2
 import pytesseract
 import subprocess
+from PIL import ImageGrab
 
 
 class QuickImage:
@@ -14,20 +15,56 @@ class QuickImage:
 
     def __init__(self):
         """Inicia a instância do objeto e configura os parâmetros iniciais."""
-        pass
+        self.text = None
+        self.words = None
+        self.img = None
 
-    def get_image(self):
+    def get_image(self, filepath=None):
         """Define a imagem, de algum arquivo ou do clipboard."""
-        pass
-
-    def read_image(self):
-        """Ler o texto contido na imagem."""
+        if filepath is None:
+            # COPIAR DO CLIPBOARD
+            img = ImageGrab.grabclipboard()
+            # or ImageGrab.grab() to grab the whole screen!
+            img.save(r'imgs\paste.png', 'PNG')
+            filepath = r'imgs\paste.png'
+            pass
+        # PEGAR DE UM LOCAL ESPECIFICO
+        self.img = cv2.imread(filepath)
         pass
 
     def get_text(self):
         """Trata o texto e retorna em forma de string."""
-        pass
+        pytesseract.pytesseract.tesseract_cmd = r"Tesseract-OCR\Tesseract.exe"
+
+        resultado = pytesseract.image_to_string(self.img)
+
+        # TRATANDO A STRING RECEBIDA
+        self.words = resultado.split()
+
+        self.text = self._process_words(self.words)
+
+        return self.text
 
     def add_to_clipboard(self):
         """Adiciona o texto para o cliboard do computador."""
+        command = 'echo ' + self.text + '|clip'
+        subprocess.run(command, shell=True)
         pass
+
+    def _process_words(self, words):
+        saida = ''
+        for word in words:
+            if word[len(word) - 1] == '-':
+                saida += word
+            else:
+                saida += word + ' '
+        return saida
+
+
+# TESTES
+if __name__ == "__main__":
+    tool = QuickImage()
+    tool.get_image()
+    tool.get_text()
+    tool.add_to_clipboard()
+    pass
